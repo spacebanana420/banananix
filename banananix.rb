be5473f12b592e2704839fd7b09ae6f020d38a06
Space Banana <tomasbessa@protonmail.com>
2023-07-05 12:20:19 +0100

    Typing non-numbers doesn't close the program

--- banananix.rb
+++ banananix.rb
@@ -11,23 +11,25 @@ while true
 4. Update system     5. Collect garbage     6. Backup home config\n\nChoose an operation"
 
     operation = gets.chomp
-    case operation.to_i
-    when 0
-        return
-    when 1
-        puts "Type the name of the package to add or type '0' to exit"; package = gets.chomp
-        if package != "0" then nixconfig_add(package) end
-    when 2
-        puts "Type the name of the package to remove or type '0' to exit"; package = gets.chomp
-        if package != "0" then nixconfig_remove(package) end
-    when 3
-        nixconfig_read()
-    when 4
-        system("sudo nixos-rebuild switch --upgrade")
-    when 5
-        system("sudo nix-collect-garbage -d")
-    when 6
-        get_config_directory()
+    if "0123456".include?(operation) == true
+        case operation.to_i
+        when 0
+            return
+        when 1
+            puts "Type the name of the package to add or type '0' to exit"; package = gets.chomp
+            if package != "0" then nixconfig_add(package) end
+        when 2
+            puts "Type the name of the package to remove or type '0' to exit"; package = gets.chomp
+            if package != "0" then nixconfig_remove(package) end
+        when 3
+            nixconfig_read()
+        when 4
+            system("sudo nixos-rebuild switch --upgrade")
+        when 5
+            system("sudo nix-collect-garbage -d")
+        when 6
+            get_config_directory()
+        end
     end
     puts ""
 end
