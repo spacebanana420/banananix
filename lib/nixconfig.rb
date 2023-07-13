4688b4a35735e27776e6d1a0965a5acdcf899d7d
Space Banana <tomasbessa@protonmail.com>
2023-07-13 12:28:56 +0100

    Per-character package detection fix

--- lib/nixconfig.rb
+++ lib/nixconfig.rb
@@ -70,7 +70,41 @@ def nixconfig_add(package)
     puts "Package '#{package}' has been added to configuration.nix"
 end
 
-def nixconfig_remove(package)
+# def nixconfig_remove(package) #read every single package char by char to fix
+#     config = File::open("/etc/nixos/configuration.nix")
+#     config_string = ""
+#     removepackages = false
+#     foundpackage = false
+#
+#     while config.eof? == false
+#         line = config.readline
+#
+#         if line.include?("environment.systemPackages") == true #needs better checking
+#             removepackages = true
+#         elsif removepackages == true && line.include?("];") == true
+#             removepackages = false
+#         end
+#
+#         if line.include?(package) == true && removepackages == true #test if line includes \n and fix detection
+#             foundpackage = true
+#             if line != package && nix_check_separate_packages(package, line) == true
+#                 config_string += line.sub(package, "")
+#             end
+#         else
+#             config_string += line
+#         end
+#     end
+#
+#     if foundpackage == true
+#         File::rename("/etc/nixos/configuration.nix", "/etc/nixos/configuration.nix.bak")
+#         File::write("/etc/nixos/configuration.nix", config_string)
+#         puts "Package '#{package}' has been removed from configuration.nix"
+#     else
+#         puts "The package '#{package}' has not been found in configuration.nix!"
+#     end
+# end
+
+def nixconfig_remove(package) #read every single package char by char to fix
     config = File::open("/etc/nixos/configuration.nix")
     config_string = ""
     removepackages = false
@@ -85,11 +119,9 @@ def nixconfig_remove(package)
             removepackages = false
         end
 
-        if line.include?(package) == true && removepackages == true #test if line includes \n and fix detection
-            foundpackage = true
-            if line != package && nix_check_separate_packages(package, line) == true
-                config_string += line.sub(package, "")
-            end
+        if line.include?(package) == true && removepackages == true
+            processed_line, foundpackage = get_line_without_package(package, line)
+            config_string += get_line_without_package(package, line)
         else
             config_string += line
         end
@@ -104,15 +136,32 @@ def nixconfig_remove(package)
     end
 end
 
-def nix_check_separate_packages(packagename, line)
+def get_line_without_package(packagename, line)
     combinedchars = ""
     for char in line.chars
-        if char == " "
-            if combinedchars == packagename then return true end
+        if char == " " || char == "\n"
+            if combinedchars == packagename
+                return line.sub(combinedchars, ""), true
+            end
             combinedchars = ""
         else
             combinedchars += char
         end
     end
-    return false
+    return line, false
 end
+
+# def nix_check_separate_packages(packagename, line)
+#     combinedchars = ""
+#     for char in line.chars
+#         if char == " "
+#             if combinedchars == packagename
+#                 return true
+#             end
+#             combinedchars = ""
+#         else
+#             combinedchars += char
+#         end
+#     end
+#     return false
+# end
